46300f61a57a82afd1363ca48acdb8377d9c3c0b
bora <bora44144@gmail.com>
2024-03-08 19:06:35 +0900

    수정

--- Calculator/Calculator(Lv3).kt
+++ Calculator/Calculator(Lv3).kt
@@ -7,43 +7,30 @@ fun main() {
         number = readLine()!!.toInt()
 
         when (number) {
-            1 -> println(AddOperation(readLine()!!.toInt(), readLine()!!.toInt()).operation())
-            2 -> println(SubstractOperation(readLine()!!.toInt(), readLine()!!.toInt()).operation())
-            3 -> println(MultiplyOperation(readLine()!!.toInt(), readLine()!!.toInt()).operation())
-            4 -> println(DivideOperation(readLine()!!.toInt(), readLine()!!.toInt()).operation())
+            1 -> println(AddOperation().operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            2 -> println(SubstractOperation().operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            3 -> println(MultiplyOperation().operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            4 -> println(DivideOperation().operation(readLine()!!.toInt(), readLine()!!.toInt()))
         }
     }
 }
 
-open class Calculator(_num1: Int, _num2: Int) {
-    var num1 = _num1
-    var num2 = _num2
-
-    open fun operation() : Int {
-        return num1 + num2
-    }
+open class Calculator() {
+    open fun operation(num1: Int, num2: Int) : Int = 0
 }
 
-class AddOperation(_num1: Int, _num2: Int) : Calculator(_num1, _num2) {
-    override fun operation(): Int {
-        return num1 + num2
-    }
+class AddOperation() : Calculator() {
+    override fun operation(num1: Int, num2: Int): Int = num1 + num2
 }
 
-class SubstractOperation(_num1: Int, _num2: Int) : Calculator(_num1, _num2) {
-    override fun operation(): Int {
-        return num1 - num2
-    }
+class SubstractOperation() : Calculator() {
+    override fun operation(num1: Int, num2: Int): Int = num1 - num2
 }
 
-class MultiplyOperation(_num1: Int, _num2: Int) : Calculator(_num1, _num2) {
-    override fun operation() : Int {
-        return num1 * num2
-    }
+class MultiplyOperation() : Calculator() {
+    override fun operation(num1: Int, num2: Int) : Int = num1 * num2
 }
 
-class DivideOperation(_num1: Int, _num2: Int) : Calculator(_num1, _num2) {
-    override fun operation() : Int {
-        return num1 / num2
-    }
+class DivideOperation() : Calculator() {
+    override fun operation(num1: Int, num2: Int) : Int = num1 / num2
 }
--- Calculator/Calculator(Lv4).kt
+++ Calculator/Calculator(Lv4).kt
@@ -2,40 +2,45 @@ package com.example.calculator
 
 fun main() {
     var number = 0
+    var addCalc = Calculator(AddOperation())
+    var subCalc = Calculator(SubstractOperation())
+    var mulCalc = Calculator(MultiplyOperation())
+    var divCalc = Calculator(DivideOperation())
 
     while (number != -1) {
         number = readLine()!!.toInt()
 
         when (number) {
-            1 -> println(Calculator(readLine()!!.toInt(), readLine()!!.toInt()).AddOperation())
-            2 -> println(Calculator(readLine()!!.toInt(), readLine()!!.toInt()).SubstractOperation())
-            3 -> println(Calculator(readLine()!!.toInt(), readLine()!!.toInt()).MultiplyOperation())
-            4 -> println(Calculator(readLine()!!.toInt(), readLine()!!.toInt()).DivideOperation())
+            1 -> println(addCalc.operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            2 -> println(subCalc.operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            3 -> println(mulCalc.operation(readLine()!!.toInt(), readLine()!!.toInt()))
+            4 -> println(divCalc.operation(readLine()!!.toInt(), readLine()!!.toInt()))
         }
     }
 }
 
-abstract class AbstractOperation {
-    abstract fun AddOperation(): Int
-    abstract fun SubstractOperation(): Int
-    abstract fun MultiplyOperation(): Int
-    abstract fun DivideOperation(): Int
+abstract class AbstractOperation() {
+    abstract fun operation(num1: Int, num2: Int): Int
 }
 
-class Calculator(_num1: Int, _num2: Int) : AbstractOperation() {
-    var num1 = _num1
-    var num2 = _num2
+class Calculator(private val operator: AbstractOperation) {
+    fun operation(num1: Int, num2: Int): Int {
+        return operator.operation(num1, num2)
+    }
+}
 
-    override fun AddOperation() : Int {
-        return num1 + num2
-    }
-    override fun SubstractOperation() : Int{
-        return num1 - num2
-    }
-    override fun MultiplyOperation() : Int{
-        return num1 * num2
-    }
-    override fun DivideOperation() : Int{
-        return num1 / num2
-    }
+class AddOperation() : AbstractOperation() {
+    override fun operation(num1: Int, num2: Int): Int = num1 + num2
+}
+
+class SubstractOperation() : AbstractOperation() {
+    override fun operation(num1: Int, num2: Int): Int = num1 - num2
+}
+
+class MultiplyOperation() : AbstractOperation() {
+    override fun operation(num1: Int, num2: Int) : Int = num1 * num2
+}
+
+class DivideOperation() : AbstractOperation() {
+    override fun operation(num1: Int, num2: Int) : Int = num1 / num2
 }
